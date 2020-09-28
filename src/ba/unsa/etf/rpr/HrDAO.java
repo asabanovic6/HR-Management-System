4182d7bd28e7aa29d4cc94ba74389230e83c9914
Amina Sabanovic <asabanovic6@etf.unsa.ba>
2020-09-28 19:02:40 +0200

    DataBase done!  HrDAO class, methodes: getInstance and regenerateDataBase done!

---
+++ src/ba/unsa/etf/rpr/HrDAO.java
@@ -0,0 +1,68 @@
+package ba.unsa.etf.rpr;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.sql.*;
+import java.util.Scanner;
+
+public class HrDAO {
+    private static HrDAO instance;
+    private Connection conn;
+    private PreparedStatement getEmployeesPS;
+
+    public static HrDAO getInstance() {
+        if (instance==null) instance= new HrDAO();
+        return  instance;
+    }
+
+    private HrDAO () {
+        try {
+            conn= DriverManager.getConnection("jdbc:sqlite.baza.db");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        try {
+            getEmployeesPS = conn.prepareStatement("SELECT * FROM employees");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+            regenerateDateBase();
+            try {
+                getEmployeesPS = conn.prepareStatement("select * from  employees");
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void regenerateDateBase() {
+        Scanner ulaz= null;
+        try {
+            ulaz = new Scanner(new FileInputStream("baza.db.sql"));
+
+            String sqlUpit = "";
+            while (ulaz.hasNext()) {
+
+                sqlUpit += ulaz.nextLine();
+                if (sqlUpit.charAt(sqlUpit.length() - 1) == ';') {
+                    try {
+                        Statement stmt = conn.createStatement();
+                        stmt.execute(sqlUpit);
+                        sqlUpit = "";
+                    }
+                    catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            ulaz.close();
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -1,3 +1,5 @@
+package ba.unsa.etf.rpr;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
